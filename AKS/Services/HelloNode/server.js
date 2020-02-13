4da76a938f6326a4f3cb39768ca66052199f25d7
Anowar Hossain <anowar.hossain@brainstation23.com>
2020-02-13 21:01:45 +0600

    update node app

--- AKS/Services/HelloNode/server.js
+++ AKS/Services/HelloNode/server.js
@@ -2,9 +2,25 @@
 var http = require('http');
 var port = process.env.PORT || 80;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('{ "message": "Hello World from Node.js updated", "port": ' + port + ' }');
+var fileSystem = require('fs');
+
+http.createServer(function (req, resp) {
+    // res.writeHead(200, { 'Content-Type': 'application/json' });
+    // res.end('{ "message": "Hello World from Node.js updated", "port": ' + port + ' }');
+
+    fileSystem.readFile('./index.html', function(error, fileContent){
+		if(error){
+			resp.writeHead(500, {'Content-Type': 'text/plain'});
+			resp.end('Error');
+		}
+		else{
+			resp.writeHead(200, {'Content-Type': 'text/html'});
+			resp.write(fileContent);
+			resp.end();
+		}
+    });
+    
+
 }).listen(port, _cb => {
     console.log('started on port: ' + port);
 });
